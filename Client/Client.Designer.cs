3f78857132008e70868f31d4ec238c4948c3bd44
tranhoanghuy123 <106876476+tranhoanghuy123@users.noreply.github.com>
2022-10-24 16:59:06 +0700

    Add files via upload

---
+++ Client/Client.Designer.cs
@@ -0,0 +1,118 @@
+﻿namespace Client
+{
+    partial class Client
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txt_Result = new System.Windows.Forms.TextBox();
+            this.btn_Send = new System.Windows.Forms.Button();
+            this.txt_Message = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btn_Clear = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txt_Result
+            // 
+            this.txt_Result.Enabled = false;
+            this.txt_Result.Location = new System.Drawing.Point(99, 88);
+            this.txt_Result.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.txt_Result.Multiline = true;
+            this.txt_Result.Name = "txt_Result";
+            this.txt_Result.Size = new System.Drawing.Size(545, 333);
+            this.txt_Result.TabIndex = 7;
+            // 
+            // btn_Send
+            // 
+            this.btn_Send.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btn_Send.Location = new System.Drawing.Point(652, 42);
+            this.btn_Send.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.btn_Send.Name = "btn_Send";
+            this.btn_Send.Size = new System.Drawing.Size(103, 27);
+            this.btn_Send.TabIndex = 6;
+            this.btn_Send.Text = "Send";
+            this.btn_Send.UseVisualStyleBackColor = true;
+            this.btn_Send.Click += new System.EventHandler(this.btn_Send_Click);
+            // 
+            // txt_Message
+            // 
+            this.txt_Message.Location = new System.Drawing.Point(99, 43);
+            this.txt_Message.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.txt_Message.Name = "txt_Message";
+            this.txt_Message.Size = new System.Drawing.Size(545, 26);
+            this.txt_Message.TabIndex = 5;
+            this.txt_Message.TextChanged += new System.EventHandler(this.txt_Message_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(15, 46);
+            this.label1.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(74, 20);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Message";
+            // 
+            // btn_Clear
+            // 
+            this.btn_Clear.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btn_Clear.Location = new System.Drawing.Point(652, 88);
+            this.btn_Clear.Name = "btn_Clear";
+            this.btn_Clear.Size = new System.Drawing.Size(105, 42);
+            this.btn_Clear.TabIndex = 8;
+            this.btn_Clear.Text = "Clear Result";
+            this.btn_Clear.UseVisualStyleBackColor = true;
+            this.btn_Clear.Click += new System.EventHandler(this.btn_Clear_Click);
+            // 
+            // Client
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(769, 451);
+            this.Controls.Add(this.btn_Clear);
+            this.Controls.Add(this.txt_Result);
+            this.Controls.Add(this.btn_Send);
+            this.Controls.Add(this.txt_Message);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "Client";
+            this.Text = "Client Chat";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txt_Result;
+        private System.Windows.Forms.Button btn_Send;
+        private System.Windows.Forms.TextBox txt_Message;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btn_Clear;
+    }
+}
+
---
+++ Client/Client.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net.Sockets;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Client
+{
+    public partial class Client : Form
+    {
+        public Client()
+        {
+            InitializeComponent();
+            ShowMessage("Chat UDP Sử Dụng Server: vpswindow10.ddns.net");
+            ShowMessage("Author: Trần Hoàng Huy ^_^");
+            ShowMessage("Connected vpswindow10.ddns.net!");
+        }
+
+        private void txt_Message_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn_Send_Click(object sender, EventArgs e)
+        {
+            Socket client = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+            IPAddress[] IPServer = Dns.GetHostAddresses("vpswindow10.ddns.net"); // this is my server
+
+            EndPoint remoteEP = new IPEndPoint(IPServer[0], 9999);
+            //Note -- need to use EndPoint for the ReceiveFrom to work!
+            byte[] data;
+            int recv;
+
+            client.SendTo(Encoding.UTF8.GetBytes(txt_Message.Text), remoteEP);
+            data = new byte[1024];
+            recv = client.ReceiveFrom(data, ref remoteEP);
+            txt_Result.Text += "Receive From " + remoteEP.ToString() + ": ";
+            ShowMessage(Encoding.UTF8.GetString(data, 0, recv));
+            //client.Close();
+        }
+        public void ShowMessage(string message)
+        {
+            txt_Result.Text += message + "\r\n";
+        }
+
+        private void btn_Clear_Click(object sender, EventArgs e)
+        {
+            txt_Result.Text = null;
+        }
+    }
+}
